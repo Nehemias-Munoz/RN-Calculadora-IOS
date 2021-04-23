35021d96c617f6d88d8510d39070ef946ff52e26
Nehemias-Munoz <nehemiasmarvic.dev@gmail.com>
2021-04-23 00:39:35 -0400

    Liempieza y creacion de archivo Calculadora Screen

---
+++ App.tsx
@@ -0,0 +1,19 @@
+ import React from 'react';
+ import {
+   StatusBar,
+   StyleSheet,
+   Text,
+   View,
+ } from 'react-native';
+
+ const App = () => {
+
+   return (
+     <View>
+       <Text>Calculadora</Text>
+     </View>
+      
+   );
+ };
+
+ export default App;
---
+++ src/screens/CalculadoraScreen.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const CalculadoraScreen = () => {
+  return(
+    <View>
+        <Text>Desde Calculadora screen</Text>
+    </View>  
+  );
+};
+ 
+export default CalculadoraScreen;
